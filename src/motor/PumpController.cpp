a445c72e8354d62919f6ca9226cbf287e5aa3f34
Chaichana Surinrach <chaichanafew2004@gmail.com>
2025-11-01 23:39:27 +0700

    Add WiFi Scanner

--- src/motor/PumpController.cpp
+++ src/motor/PumpController.cpp
@@ -6,49 +6,64 @@ const unsigned int MIN_SOIL_MOISTURE = 0;
 const unsigned int PUMP_RUN_TIME = 2000;
 const unsigned int PUMP_WAIT_TIME = 2000;
 
-enum PumpState {IDLE, RUN, WAIT};
+enum PumpState
+{
+    IDLE,
+    RUN,
+    WAIT
+};
 PumpState pumpState = IDLE;
 
-PumpController::PumpController(int pin):
-    relayPin(pin)
-{}
+PumpController::PumpController(int pin) : relayPin(pin)
+{
+}
 
-void PumpController::begin(){
+void PumpController::begin()
+{
     pinMode(relayPin, OUTPUT);
-    digitalWrite(relayPin, LOW);    
-}
-
-void PumpController::turnOn(){
-    digitalWrite(relayPin, HIGH);
-}
-
-void PumpController::turnOff(){
     digitalWrite(relayPin, LOW);
 }
 
-void PumpController::setSoilMoisture(int moisture, int target){
+void PumpController::turnOn()
+{
+    digitalWrite(relayPin, HIGH);
+}
+
+void PumpController::turnOff()
+{
+    digitalWrite(relayPin, LOW);
+}
+
+void PumpController::setSoilMoisture(int moisture, int target)
+{
     unsigned long PumpStartTime = 0;
     unsigned long pumpCyclewait = 0;
-    
+
+    Serial.println(moisture);
+    Serial.println(target);
+
     unsigned long now = millis();
     switch (pumpState)
     {
     case IDLE:
-        if(moisture < target){
+        if (moisture < target)
+        {
             turnOn();
             pumpState = RUN;
             PumpStartTime = now;
         }
         break;
-    case RUN: 
-        if(now - PumpStartTime >= PUMP_RUN_TIME){
+    case RUN:
+        if (now - PumpStartTime >= PUMP_RUN_TIME)
+        {
             turnOff();
             pumpState = WAIT;
             pumpCyclewait = now;
         }
         break;
     case WAIT:
-        if(now - PumpStartTime >= PUMP_WAIT_TIME) {
+        if (now - PumpStartTime >= PUMP_WAIT_TIME)
+        {
             pumpState = IDLE;
         }
         break;
